7232c7e4b0b67d06a90d4a74a15211488fb9a912
Anirudha Kanade <anirudhakanade1007@gmail.com>
2025-01-05 07:13:10 +0530

    code correction

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,33 +6,40 @@ namespace SKMWebsite.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Active = "home";
             return View();
         }
 
         public ActionResult AboutUs()
         {
+            ViewBag.Active = "AboutUs";
             return View();
         }
         public ActionResult OurServices()
         {
+            ViewBag.Active = "OurServices";
             return View();
         }
         public ActionResult Calculator(string loanType = null)
         {
+            ViewBag.Active = "Calculator";
             loanType = string.IsNullOrEmpty(loanType) ? "home" : loanType;
             return View("~/Views/Home/Calculator.cshtml", null, loanType);
         }
         public ActionResult Downloads()
         {
+            ViewBag.Active = "Downloads";
             return View();
         }
         public ActionResult Reviews()
         {
+            ViewBag.Active = "Reviews";
             return View();
         }
 
         public ActionResult ContactUs()
         {
+            ViewBag.Active = "ContactUs";
             return View();
         }
     }
